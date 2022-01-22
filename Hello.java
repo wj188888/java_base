b0a85df06e9aba26cbd79648f268e90d813d7eb1
wangjie <2460665525@qq.com>
2022-01-22 21:44:47 +0800

    java基础hello world

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        // 单行注释，输出一个hello， world！
+        System.out.println("Hello World!!!");
+        /*
+        多行注释：
+        你好的
+        我是谁啊
+        */
+    }
+}
