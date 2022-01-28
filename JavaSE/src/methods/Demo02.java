432daa4a0c5d4cb4250e930da6f3d602b0c3ecc6
wangjie <2460665525@qq.com>
2022-01-28 19:17:03 +0800

    方法，重载，等命令行参数等可变参数等

---
+++ JavaSE/src/methods/Demo02.java
@@ -0,0 +1,39 @@
+package methods;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        int results = max(3, 3);
+        System.out.println(results);
+        double res = max(1.232, 1.212);
+        System.out.println(res);
+    }
+    // 比大小
+    public static int max(int numone, int numtwo){
+        int result = 0;
+
+        if (numone > numtwo) {
+            return numone;
+        } else if (numone == numtwo) {
+            return 0;
+        }
+        else {
+            return result;
+        }
+
+
+    }
+
+    // 重载
+    public static double max(double numone, double numtwo){
+        int result = 0;
+
+        if (numone > numtwo) {
+            return numone;
+        } else if (numone == numtwo) {
+            return 0;
+        }
+        else {
+            return result;
+        }
+    }
+}
---
+++ JavaSE/src/methods/Demo04.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class Demo04 {
+    public static void main(String[] args) {
+
+
+    }
+    // 可变参数；也叫作:不定项参数
+
+}
