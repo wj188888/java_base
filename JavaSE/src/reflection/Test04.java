f3300842fe876b8190ba096527f307ba01c304de
wangjie <2460665525@qq.com>
2022-02-08 18:10:39 +0800

    类的加载

--- JavaSE/src/reflection/Test04.java
+++ JavaSE/src/reflection/Test04.java
@@ -1,14 +1,35 @@
 package reflection;
 
+import java.lang.annotation.ElementType;
+
 // 所有类型的class
 public class Test04 {
     public static void main(String[] args) {
-        Class c1 = Object.class;
-        Class c2 = Comparable.class;
-        Class c3 = String.class;
-        Class c4 = int[][].class;
-        Class c5 = Override.class;
+        Class c1 = Object.class; // 类
+        Class c2 = Comparable.class; // 接口
+        Class c3 = String[].class; // 一维数组
+        Class c4 = int[][].class; // 二维数组
+        Class c5 = Override.class; // 注解
+        Class c6 = ElementType.class;  // 注解的类annotation.ElementType
+        Class c7 = Integer.class; // 基本数据类型
+        Class c8 = void.class; // void
+        Class c9 = Class.class; // Class
 
+        // 只要元素类型与维度一样就是同一个Class.
+        int[] a = new int[10];
+        int[] b = new int[100];
+        System.out.println(a.getClass().hashCode());
+        System.out.println(b.getClass().hashCode());
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(c7);
+        System.out.println(c8);
+        System.out.println(c9);
 
     }
 }
