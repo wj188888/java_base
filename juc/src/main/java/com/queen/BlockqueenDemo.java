a0c97ffa36c6615f43d509e2d19e79d5169fa734
wangjie <2460665525@qq.com>
2022-05-11 17:42:27 +0800

    JUC队列显示

--- juc/src/main/java/com/queen/BlockqueenDemo.java
+++ juc/src/main/java/com/queen/BlockqueenDemo.java
@@ -1,4 +1,15 @@
 package com.queen;
 
 public class BlockqueenDemo {
+    public static void main(String[] args) {
+        test1();
+    }
+    public static void blockingQueue.add("C")(){
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<>(3);
+
+        System.out.println(blockingQueue.add("A"));
+        System.out.println(blockingQueue.add("B"));
+        System.out.println(blockingQueue.add("C"));
+        System.out.println(blockingQueue.add("D"));
+    }
 }
