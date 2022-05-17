cf7655da8d65716f8c227dbfd807238805a9e705
wangjie <2460665525@qq.com>
2022-05-17 23:40:20 +0800

    juc学习完成+1,需要不断练习

---
+++ juc/src/main/java/com/lock/Demo01.java
@@ -0,0 +1,26 @@
+package com.lock;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+
+        new Thread(() -> {
+            phone.sms();
+        },"A").start();
+
+        new Thread(() -> {
+            phone.sms();
+        },"B").start();
+    }   
+}
+
+class Phone{
+    public synchronized void sms() {
+        System.out.println(Thread.currentThread().getName()+" sms");
+        call();
+    }
+
+    public synchronized void call() {
+        System.out.println(Thread.currentThread().getName() + " call");
+    }
+}
