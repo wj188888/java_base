18705cd8c748a03e6afbc6dd3d2ea2f6f7c0bd9c
jie.wang <18428333658@163.com>
2023-09-22 21:50:56 +0800

    回顾LinkList和ArrayList数组，LinkList的删除和添加比较厉害

--- JavaSE/src/com/wangjie/Base01/ArrayListDemo01.java
+++ JavaSE/src/com/wangjie/Base01/ArrayListDemo01.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 public class ArrayListDemo01 {
     public static void main(String[] args) {
+        // 定义一维数组intList，数据为integer包类型
         ArrayList<Integer> intList = new ArrayList<Integer>();
         intList.add(123);
         intList.add(456);
         intList.add(789);
+        intList.add(128312);
         for (Integer integer : intList) {
             System.out.println(integer);
         }
--- JavaSE/src/com/wangjie/Base01/LinkListDemo01.java
+++ JavaSE/src/com/wangjie/Base01/LinkListDemo01.java
@@ -9,14 +9,25 @@ public class LinkListDemo01 {
         LinkedList<String> list2 = new LinkedList<String>();
         Arrays.asList(new String[]{"a","b","c"});
 
-        list.add("1231231szz");
-        list.add("1231231szz");
-        list.offer("1231231szz");
+        list.add("1231231szz"); // 在尾部添加
+        list.add("1231231szzbbb");
+        list.offer("1231231szzkkk"); // 在尾部添加
         for (String s : list) {
             System.out.println(s);
         }
         System.out.println(list);
-
-        list.element();
+        list.addLast("131saa"); // 在最后一个添加
+        list.addFirst("888111eacx"); // 在首部添加一个
+        LinkedList<String> l3 = (LinkedList<String>) list.clone(); // 复制list
+        System.out.println("l3: " + l3);
+        System.out.println("list: " + list);
+        System.out.println("list双向链表第一个" + list.getFirst());
+        //list.clear(); // 清空
+        System.out.println("list: " + list);
+        //System.out.println(list.getFirst()); // 如果list为空，报错NoSuchElementException
+        System.out.println("查看list的值的下标值：" + list.indexOf("1231231szz"));
+        System.out.println(list.peek()); // 获取这个list的第一个值
+        System.out.println(list);
+        list.poll(); // 检索并删除list的第一个值
     }
 }
